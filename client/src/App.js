833fea9385a68231611d3567bafa4793fda1bc0c
arti-yadav18 <yarti7065@gmail.com>
2025-06-12 15:13:16 +0200

    Add proper .gitignore files for client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Food Scanner</h1>
+      <p>Search or scan a food item to get nutritional info.</p>
     </div>
   );
 }
-
 export default App;
